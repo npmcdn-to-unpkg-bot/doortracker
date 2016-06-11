57625b8262e21f5f0b755b05e03ac0f7152c563c
Dimitri Who <dimitri.who@gmail.com>
2016-06-11 22:13:06 +0200

    Added get/add/edit/delete basic methods of Api; Removed check token csrf for api

--- app/Http/Controllers/Api/Version1/Controller.php
+++ app/Http/Controllers/Api/Version1/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Version1;
 
 use App\Http\Controllers\Api\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController{
 
@@ -23,9 +24,11 @@ class Controller extends BaseController{
 	/**
 	 * Retrieve all data
 	 *
+	 * @param Request $request
+	 *
 	 * @return Response
 	 */
-	public function all(){
+	public function all(Request $request){
 		
 		$schema = $this -> getSchema();
 		$resources = call_user_func($this -> model.'::select',$schema -> getFieldsName()) -> get() -> toArray();
@@ -33,6 +36,102 @@ class Controller extends BaseController{
 		$response = [
 			'status' => 'Success',
 			'data' => $resources
+		];
+
+		return $this -> json($response);
+
+	}
+
+	/**
+	 * Get
+	 *
+	 * @param int $id
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function get($id,Request $request){
+		
+		$schema = $this -> getSchema();
+
+
+		$resource = call_user_func($this -> model.'::where',['id' => $id]) -> first();	
+
+		$response = [
+			'status' => 'Success',
+			'data' => $resource -> toArray()
+		];
+
+		return $this -> json($response);
+
+	}
+
+	/**
+	 * Add
+	 *
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function add(Request $request){
+		
+		$schema = $this -> getSchema();
+
+		$fields = $request -> all();
+		$resource = call_user_func($this -> model.'::create',$fields);
+
+		$response = [
+			'status' => 'Success',
+			'data' => $resource -> toArray()
+		];
+
+		return $this -> json($response);
+
+	}
+
+	/**
+	 * Edit
+	 *
+	 * @param int $id
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function edit($id,Request $request){
+		
+		$schema = $this -> getSchema();
+
+		$fields = $request -> all();
+		$resource = call_user_func($this -> model.'::where',['id' => $id]) -> first();
+
+		$resource -> fill($fields);
+		$resource -> save();
+
+		$response = [
+			'status' => 'Success',
+			'data' => $resource -> toArray()
+		];
+
+		return $this -> json($response);
+
+	}
+
+	/**
+	 * Delete
+	 *
+	 * @param int $id
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function delete($id,Request $request){
+		
+		$schema = $this -> getSchema();
+
+		$resource = call_user_func($this -> model.'::where',['id' => $id]) -> delete();
+
+		$response = [
+			'status' => 'Success'
 		];
 
 		return $this -> json($response);
--- app/Http/Controllers/Api/Version1/IssueController.php
+++ app/Http/Controllers/Api/Version1/IssueController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api\Version1;
 
+use Illuminate\Http\Request;
+
 
 class IssueController extends Controller{
 
@@ -20,12 +22,62 @@ class IssueController extends Controller{
 	protected $__schema = 'App\Schema\Issue';
 
 	/**
-	 * Retrieve all data
+	 * All
+	 *
+	 * @param Request $request
 	 *
 	 * @return Response
 	 */
-	public function all(){
-		return parent::all();
+	public function all(Request $request){
+		return parent::all($request);
 	}
+
+	/**
+	 * Get
+	 *
+	 * @param int $id
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function get($id,Request $request){
+		return parent::get($id,$request);
+	}
+
+	/**
+	 * Add
+	 *
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function add(Request $request){
+		return parent::add($request);
+	}
+
+	/**
+	 * Edit
+	 *
+	 * @param int $id
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function edit($id,Request $request){
+		return parent::edit($id,$request);
+	}
+
+	/**
+	 * Delete
+	 *
+	 * @param int $id
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function delete($id,Request $request){
+		return parent::delete($id,$request);
+	}
+
 
 }
